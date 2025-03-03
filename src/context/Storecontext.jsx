26c4a812437b5b2a66531b0a2beb2d14f5b9a530
Rabeeh-Ashraf <rabeeh.ashraf2002@gmail.com>
2025-03-03 16:32:32 +0530

    add to cart logic

--- src/context/Storecontext.jsx
+++ src/context/Storecontext.jsx
@@ -3,6 +3,16 @@ import { food_list } from "../assets/assets";
 
 export const Storecontext = createContext(null)
 const Storecontextprovider = (props)=>{
+
+    const [cartitems,setcartitems]=useState({})
+    const addtocart =(itemId)=>{
+        if(!cartitems[itemId]){
+            setcartitems((prev)=>({...prev,[itemId]:1}))
+        }
+        else{
+            setcartitems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+        }
+    }
     const Contextvalue = {
         food_list
     }

fe937bc2e5def8404db1332e9cfaf1ba099e286c
Rabeeh-Ashraf <rabeeh.ashraf2002@gmail.com>
2025-03-04 12:31:46 +0530

    displaying  item matches to its category

--- src/components/Fooddisplay/Fooddisplay.jsx
+++ src/components/Fooddisplay/Fooddisplay.jsx
@@ -2,17 +2,23 @@ import React, { useContext } from 'react'
 import { Storecontext } from '../../context/Storecontext'
 import Fooditem from '../fooditem/Fooditem'
 import ('./fooddisplay.css')
+
+
 const Fooddisplay = ({category}) => {
     const {food_list}=useContext(Storecontext)
   return (
     <div className='fooddisplay' id='fooddisplay'>
         <h2>Top dishes near you</h2>
         <div className='food-display-list'>
-            {food_list.map((item,index)=>{
-                return <Fooditem key={index}id={item._id}name={item.name}description={item.description}price={item.price}image={item.image}/>
-            })}
+              {food_list.map((item,index)=>{
+              if(category==="All" || category===item.category){
+                  return <Fooditem key={index}id={item._id}name={item.name}description={item.description}price={item.price}image={item.image}/>
+                }
+              }
+            )
+          }
         </div>
-        </div>
+    </div>
   )
 }
 

bd9fe15c5ec8c93a9815d6b2dca9203333ba779c
Rabeeh-Ashraf <rabeeh.ashraf2002@gmail.com>
2025-03-11 17:20:26 +0530

    updated redirecting of page when clicking on basket icon

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './cart.css'
+import { Storecontext } from '../../context/Storecontext'
 const Cart = () => {
+  const {cartitems,food_list,removefromcart } = useContext(Storecontext)
   return (
-    <div>cart</div>
+    <div className='cart'>
+      <div className="cart-items">
+        <div className='cart-items-title'>
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+      </div>
+    </div>
   )
 }
 
